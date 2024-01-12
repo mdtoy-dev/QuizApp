f567c6c491267a6323251b3f77287cf3d4c1f077
Berk Toy <berk.toy1@gmail.com>
2024-01-12 23:59:22 +0000

    display further questions and endQuiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,15 +1,17 @@
+var currentQuestionIndex = 0;
+
 document.getElementById('start').addEventListener('click', startQuiz);
 
 function startQuiz() {
   startTimer();
-  displayQuestion(quizQuestions[0]);
+  displayQuestion(quizQuestions[currentQuestionIndex]);
 
   document.getElementById('start-screen').classList.add('hide');
   document.getElementById('questions').classList.remove('hide');
 }
 
 function startTimer() {
-  
+
 }
 
 function displayQuestion(question) {
@@ -19,14 +21,38 @@ function displayQuestion(question) {
   questionTitle.textContent = question.question;
   choicesContainer.innerHTML = '';
 
-  question.options.forEach((option, index) => {
+  question.options.forEach(function(option) {
     var choiceButton = document.createElement('button');
     choiceButton.textContent = option;
-    choiceButton.addEventListener('click', () => handleAnswerClick(option));
+    choiceButton.addEventListener('click', function() {
+      handleAnswerClick(option);
+    });
     choicesContainer.appendChild(choiceButton);
   });
 }
 
 function handleAnswerClick(selectedAnswer) {
+  var currentQuestion = quizQuestions[currentQuestionIndex];
 
+  if (selectedAnswer === currentQuestion.correctAnswer) {
+    console.log("Correct!");
+  } else {
+    console.log("Incorrect!");
+  }
+
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex < quizQuestions.length) {
+    displayQuestion(quizQuestions[currentQuestionIndex]);
+  } else {
+    endQuiz();
+  }
+}
+
+function endQuiz() {
+  document.getElementById('questions').classList.add('hide');
+  document.getElementById('end-screen').classList.remove('hide');
+
+  var finalScore = calculateFinalScore(); 
+  document.getElementById('final-score').textContent = finalScore;
 }
